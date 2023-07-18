c8203dd799140b7013ab58c2a9a0dbff9d0536f0
vfornerprofesor <91624772+vfornerprofesor@users.noreply.github.com>
2023-07-18 12:35:34 +0200

    Add files via upload

---
+++ scripts/utils.js
@@ -0,0 +1,39 @@
+function processStyles(element, styles) {
+    if (styles && styles.length > 0) {
+        styles = styles.split(';');
+        for (let s of styles) {
+            let style = s.split(':');
+            if (style.length == 2) {
+                element.style[style[0].trim()] = style[1].trim();
+            }
+        }
+    }
+    return element;
+}
+
+function processClasses(element, classes) {
+    if (classes && classes.length > 0) {
+        classes = classes.split(' ');
+        for (let c of classes) {
+            element.classList.add(c);
+        }
+    }
+    return element;
+}
+
+function processTextBoldAndLinks(text) {
+    // Reemplaza los asteriscos (*) por etiquetas <strong> para el texto en negrita
+    const formattedText = text.replace(/\*(.*?)\*/g, '<strong>$1</strong>');
+    // Reemplaza los corchetes [] por etiquetas <a> para los enlaces
+    const finalText = formattedText.replace(
+        /\[(.*?)\]\((.*?)\)/g,
+        '<a href="$2" target="_blank">$1</a>'
+    );
+    return finalText;
+}
+
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
