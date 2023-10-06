79a05527bfd2981396d2af80b4bc8950dba21b4a
vfornerprofesor <vforner.profesor@gmail.com>
2023-10-06 10:40:25 +0200

    php

---
+++ unitats/programacio/web/php/exercises/05-forms/ex1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercici 1</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        body {
+            margin-top: 50px;
+            text-align: center;
+        }
+
+        body div {
+            width: 50%;
+            margin: 0 auto;
+            padding: 40px;
+            background-color: khaki;
+            color: darkslategrey;
+        }
+
+        ul {list-style: none;}
+    </style>
+</head>
+
+<body>
+
+    <div>
+        <?php
+
+        //Iniciar o reanudar la sessió
+
+        
+        //Verificar si existeix una variable de sessió paraules
+        //  inicialitzar paraules a una llista buida
+
+
+        //En METHOD POST
+        //  Obtindre paraula i traducció
+        //  Afeigir la paraula a la variable paraules de la sessió
+            
+        ?>
+
+        <!-- CREAR TÍTOL 2 -->
+        <!-- CREAR FORMULARI -->
+
+        <?php
+        
+        // Mostrar les paraules si existeix la variable paraules en la sessió
+       
+
+        //Destruir la sessió per esborrar les variables
+        
+        ?>
+    </div>
+</body>
+
+</html>
