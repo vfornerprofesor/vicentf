01fe47f7bbe1ad3ffe30acc0e8824d624ba3780d
vfornerprofesor <vforner.profesor@gmail.com>
2025-04-07 19:34:36 +0200

    Escaperoom

--- components/vf-img.js
+++ components/vf-img.js
@@ -12,29 +12,29 @@ class VFImage extends HTMLElement {
     }
 
     render() {
-        
+
         let a;
-        if(this.getAttribute('link') && this.getAttribute('link') != "") {
+        if (this.getAttribute('link') && this.getAttribute('link') != "") {
             a = document.createElement('a');
             a.href = this.getAttribute('link');
             //a.target = "_blank";
         };
 
-      let img = document.createElement('img');
-      img.src = this.getAttribute('src');
-      img = processClasses(img, this.getAttribute('classes'));
-      img = processStyles(img, this.getAttribute('styles'));
-      //img.classList.add("img-fluid");
-      img.classList.add("my-3");
-      img.classList.add("center");
-      img.classList.add("vf-img");
+        let img = document.createElement('img');
+        img.src = this.getAttribute('src');
+        img = processClasses(img, this.getAttribute('classes'));
+        img = processStyles(img, this.getAttribute('styles'));
+        //img.classList.add("img-fluid");
+        img.classList.add("my-3");
+        img.classList.add("center");
+        img.classList.add("vf-img");
 
-      if(a) {
-        a.appendChild(img);
-        this.appendChild(a);
-      } else {
-          this.appendChild(img);
-      }
+        if (a) {
+            a.appendChild(img);
+            this.appendChild(a);
+        } else {
+            this.appendChild(img);
+        }
     }
 
 
